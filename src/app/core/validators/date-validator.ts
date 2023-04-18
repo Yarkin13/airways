43b73eae860a51f78c6f625ca4d74e16278f2e70
Яркин Станислав Анатольевич <SYArkin@inno.local>
2023-04-18 14:07:41 +0400

    fix: after review

--- src/app/core/validators/date-validator.ts
+++ src/app/core/validators/date-validator.ts
@@ -2,10 +2,13 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export function dateValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
+    if (!Date.parse(control.value)) return { dateValidator: { value: control.value } };
+
     const inputDate = new Date(control.value);
-    if (!inputDate) return { dateValidator: { value: control.value } };
     const currentYear = new Date().getFullYear();
+    // must be over 18 years of age
     const maxDate = new Date(currentYear - 18, 0, 0).getTime();
+    // must be under 90 years of age
     const minDate = new Date(currentYear - 90, 0, 0).getTime();
     const inputDateInMS = inputDate.getTime();
 
