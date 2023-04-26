55033cb161a99c0593d8786bbd0a8e98875f0ac1
Natallia Chinarova <philonena7@gmail.com>
2023-04-26 14:15:48 +0300

    feat: add date format

--- src/app/booking/pages/main/reverses-button/reverses-button.component.ts
+++ src/app/booking/pages/main/reverses-button/reverses-button.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'reverses-button',
   templateUrl: './reverses-button.component.html',
   styleUrls: ['./reverses-button.component.scss'],
 })
-export class ReversesButtonComponent {}
+export class ReversesButtonComponent {
+  @Input() flightSearchForm!: FormGroup;
+
+  reverseDirection() {
+    const controlTo = this.flightSearchForm.get('from');
+    const controlBack = this.flightSearchForm.get('destination');
+
+    const valueTo = controlTo?.value;
+    const valueBack = controlBack?.value;
+
+    controlTo?.setValue(valueBack);
+    controlBack?.setValue(valueTo);
+  }
+}
